42ff8ab209568e2e689cff30fa401a3f4652d717
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2023-04-17 22:14:05 -0300

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import { useState } from "react";
-import Container from "./components/Container";
-import Spinner from "./components/Spinner";
+import { useState } from 'react';
+import Container from './components/Container';
+import Spinner from './components/Spinner';
 
 function App() {
   const [quote, setQuote] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleClick = async (event) => {
-    setIsLoading(true)
-    // TODO
-    setIsLoading(false)
-  }
+    setIsLoading(true);
+    const response = await fetch('https://meowfacts.herokuapp.com/');
+    const { data } = await response.json();
+
+    setQuote(data[0]);
+
+    setIsLoading(false);
+  };
 
   return (
     <Container>
-      <button data-testid="button" onClick={e => handleClick(e)}>
+      <button data-testid='button' onClick={(e) => handleClick(e)}>
         <span>get a fact</span>
       </button>
-      {isLoading || quote === '' ? 
-        ( <Spinner /> ) : ( <span data-testid="quote">{quote}</span> )
-      }
+      {isLoading || quote === '' ? (
+        <Spinner />
+      ) : (
+        <span data-testid='quote'>{quote}</span>
+      )}
     </Container>
   );
 }
